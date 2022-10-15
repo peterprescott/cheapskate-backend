26a5a38b0cdf6be634dbb0c84e9b2b5a7c38604a
Peter Prescott <prescott.peter@gmail.com>
2022-10-15 15:34:02 +0100

    See what flasgger does with docstrings

--- flask_app.py
+++ flask_app.py
@@ -7,17 +7,41 @@ app = Flask(__name__)
 swagger = Swagger(app)
 
 @app.route('/')
-def hello_world():
+def hello_world() -> str:
+    """
+    Just get started.
+
+    Returns
+    -------
+    str
+        A simple greeting
+    """
     return 'Hello from Flask!'
 
 
 @app.route('/cicd')
-def cicd():
-    return 'Maybe maybe working!'
+def cicd() -> str:
+    """
+    Check CI/CD status.
+
+    Returns
+    -------
+    str
+        A text string that you might update to prove that it changes.
+    """
+    return 'Working!'
 
 
 @app.route('/git', methods=['GET','POST'])
-def pull():
+def pull() -> str:
+    """
+    Trigger git pull.
+
+    Returns
+    -------
+    str
+      Confirmation that update was successful.
+    """
     repo = git.Repo(os.path.join('~','cheapskate-backend'))
     repo.remotes.origin.pull()
     return 'Updated'
